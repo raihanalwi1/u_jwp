42dfc00ac0fda4ff127b497c472e6505abee113b
raihanalwi1 <raihanalwi68@gmail.com>
2023-07-09 21:27:08 +0700

    add file db

---
+++ db_jewepe.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 09 Jul 2023 pada 16.26
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_jewepe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id_jadwal` int(5) NOT NULL,
+  `id_kursus` int(5) NOT NULL,
+  `waktu` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jadwal`
+--
+
+INSERT INTO `jadwal` (`id_jadwal`, `id_kursus`, `waktu`) VALUES
+(1, 1, '2022-08-05');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kursus`
+--
+
+CREATE TABLE `kursus` (
+  `id_kursus` int(5) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `lama` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kursus`
+--
+
+INSERT INTO `kursus` (`id_kursus`, `nama`, `keterangan`, `lama`) VALUES
+(1, 'Fundamental Web', 'Introduction of Web Programming', '8 pertemuan'),
+(2, 'Fundamental Golang', 'Indroduction of golang programming', '8 pertemuan'),
+(4, 'UI /UX ', 'Indroduction of User interface and User experience', '9 pertemuan'),
+(10, 'Quality Assurance', 'belajar qa bersama dari dasar ', '8 pertemuan'),
+(15, 'Cloud computing', 'learn CC', '3 pertemuan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `npm` varchar(8) NOT NULL,
+  `kelas` varchar(5) NOT NULL,
+  `nama` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `kelas`, `nama`) VALUES
+('123', '4IA01', 'Achwall Taufiq Hidayat');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `id_daftar` int(5) NOT NULL,
+  `id_jadwal` int(5) NOT NULL,
+  `npm` varchar(8) NOT NULL,
+  `krs` varchar(255) NOT NULL,
+  `status` enum('Menunggu Persetujuan','Pendaftaran Diterima','Pendaftaran Ditolak') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pendaftaran`
+--
+
+INSERT INTO `pendaftaran` (`id_daftar`, `id_jadwal`, `npm`, `krs`, `status`) VALUES
+(1, 1, '123', '010822_50418093_KRS.pdf', 'Menunggu Persetujuan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(5) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `nama` varchar(32) NOT NULL,
+  `telepon` varchar(13) NOT NULL,
+  `level` enum('Admin','Mahasiswa') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `telepon`, `level`) VALUES
+(1, 'admin', 'admin', 'alwi', '081234567890', 'Admin'),
+(2, 'andis', '1', 'Sylandis', '', 'Mahasiswa'),
+(3, 'mahasiswa1', '', 'dinda', '0923183912', 'Admin'),
+(4, 'dadas', '', 'dada', '1111', 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_kursus` (`id_kursus`);
+
+--
+-- Indeks untuk tabel `kursus`
+--
+ALTER TABLE `kursus`
+  ADD PRIMARY KEY (`id_kursus`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+
+--
+-- Indeks untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`id_daftar`),
+  ADD KEY `id_jadwal` (`id_jadwal`),
+  ADD KEY `npm` (`npm`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `id_jadwal` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `kursus`
+--
+ALTER TABLE `kursus`
+  MODIFY `id_kursus` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  MODIFY `id_daftar` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_kursus`) REFERENCES `kursus` (`id_kursus`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pendaftaran_ibfk_2` FOREIGN KEY (`npm`) REFERENCES `mahasiswa` (`npm`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
